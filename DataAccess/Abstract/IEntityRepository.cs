60bbb762a34489051d5577456d65c32f91c96d91
zeynep yalınız <zeynepyaliniz7@gmail.com>
2021-02-03 21:38:52 +0300

    repo

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -9,7 +9,7 @@ namespace DataAccess.Abstract
     {
         List<T> GetAll(Expression<Func<T,bool>> filter = null); // expression yapısı p=>p.Id yi kullanabilmek için Expression yapısı 
         // refactoring
-        T Get();
+        T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
